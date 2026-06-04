4d4c683608a563056b24a5485100f989d21b03d6
Nandani khatri <nandanikhatri228@gmail.com>
2026-06-04 20:20:01 +0530

    Updated student result program

--- labwork/iteration_4th_une_lab/student_result.py
+++ labwork/iteration_4th_une_lab/student_result.py
@@ -1,29 +1,50 @@
 #Accept marks of 5 subjects., total marks, percentage, grade, number of subject failed 
-
-total = 0
-failed_subjects = 0
-
-for i in range(1, 6):
-    marks = float(input(f"Enter marks of Subject {i}: "))
-    total += marks
-
-    if marks < 40:
-        failed_subjects += 1
-
-percentage = total / 5
-
-if percentage >= 90:
-    grade = "A+"
-elif percentage >= 75:
-    grade = "A"
-elif percentage >= 60:
-    grade = "B"
-elif percentage >= 40:
-    grade = "C"
+#----------------------------------
+#Accept/input  the 5 students marks
+failed = 0
+sub1 = int(input("Enter the 1st subject marks:")) 
+#validation
+if (sub1<0):
+    print("Marks should not be negative")
+elif(sub1 < 40):
+     failed += 1
+sub2 = int(input("Enter the 2nnd subject marks:")) 
+#validation
+if (sub2<0):
+    print("Marks should not be negative")
+elif(sub2 < 40):
+     failed += 1    
+sub3 = int(input("Enter the 3th subject marks:")) 
+#validation
+if (sub3<0):
+    print("Marks should not be negative")
+elif(sub3 < 40):
+     failed += 1    
+sub4 = int(input("Enter the 4th subject marks:")) 
+#validation
+if (sub4<0):
+    print("Marks should not be negative")
+elif(sub4 < 40):
+     failed += 1
+sub5 = int(input("Enter the 5th subject marks:")) 
+#validation
+if (sub5<0):
+    print("Marks should not be negative")
+elif(sub5 < 40):
+     failed += 1    
+total_marks = 0
+total_marks = sub1+sub2+sub3+sub4+sub5
+print("Total Marks:",total_marks)
+percentage = (total_marks/500)*100
+print("Percentage",percentage,"%")
+if(percentage >=90):
+        print("Grade: A+")
+elif(percentage>=75 and percentage<90):
+        print("Grade: A")    
+elif(percentage>=60 and percentage<75):
+        print("Grade: B")
+elif(percentage>=40 and percentage<60):
+        print("Grade:C")
 else:
-    grade = "Fail"
-
-print("\nTotal Marks =", total)
-print("Percentage =", percentage, "%")
-print("Grade =", grade)
-print("Number of Subjects Failed =", failed_subjects)
+        print("Fail")     
+print("Number of Subjects Failed:", failed)            
