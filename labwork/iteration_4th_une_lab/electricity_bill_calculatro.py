686b427b37859bf1ffc79fe500bb42891039f296
Nandani khatri <nandanikhatri228@gmail.com>
2026-06-04 19:39:58 +0530

    Added electricity bill calculation program

---
+++ labwork/iteration_4th_une_lab/electricity_bill_calculatro.py
@@ -0,0 +1,18 @@
+#electricity bill 
+unit = int(input("enter the unit consumed"))
+#validation of input
+if (unit<0):
+    print("electricity unit should not be negative")
+#--------------------------------------------
+# calculation for bills 
+# calculation  of bill when unit is between 0 to 100    
+elif(unit<=100):
+    bill = unit*100
+#calculation of bill when unit is between 101 to 200    
+elif(unit<=200):
+    bill = (100*5)+((unit-100)*7)
+#calculation of bill when unit is more than 200    
+else:
+    bill = (100*5)+(100*7)+((unit-200)*10)
+print("Units Consumed:",unit)
+print("Total Bill:",bill)
