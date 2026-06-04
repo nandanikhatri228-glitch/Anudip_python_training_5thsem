fde63dfa05ed778b63994dbb7649c2f0c41a609e
Nandani khatri <nandanikhatri228@gmail.com>
2026-06-04 19:46:06 +0530

    Added category in electricity bill program

--- labwork/iteration_4th_une_lab/electricity_bill_calculatro.py
+++ labwork/iteration_4th_une_lab/electricity_bill_calculatro.py
@@ -7,12 +7,16 @@ if (unit<0):
 # calculation for bills 
 # calculation  of bill when unit is between 0 to 100    
 elif(unit<=100):
-    bill = unit*100
+    bill = unit*5
+    category = "Low Consumption"
 #calculation of bill when unit is between 101 to 200    
 elif(unit<=200):
     bill = (100*5)+((unit-100)*7)
+    category = "Medium Consumption"
 #calculation of bill when unit is more than 200    
 else:
     bill = (100*5)+(100*7)+((unit-200)*10)
+    category = "High Consumption"
 print("Units Consumed:",unit)
 print("Total Bill:",bill)
+print("Category:",category)
